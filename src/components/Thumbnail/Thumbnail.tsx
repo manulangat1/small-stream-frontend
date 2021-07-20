054706bfc776db5c831d3b54bbab12a6189a8fa3
manulangat1 <emmanuelthedeveloper.com>
2021-07-21 01:34:27 +0300

    feat:adding-youtube-feature
    Added the trailer functionality by the use of the react - youtube liblary

--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ThumbUpIcon} from '@heroicons/react/outline'
+import axios from 'axios'
+import Youtube from 'react-youtube'
 interface thumbNailI{
     result:any
 }
 const  Thumbnail = ({result}:thumbNailI)  => {
-    console.log(result)
+    // console.log(result)
+    const opts = {
+        height: '390',
+        width: '640',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+      };
     const BASE_URL = 'https://image.tmdb.org/t/p/original/'
+    const [show,setShow] = useState<boolean>(false)
+    const [trailerLink,setTrailerLink] = useState<string>('')
+    useEffect(() => {
+        async function getTrailer(){
+            const resd = await axios.get(`https://api.themoviedb.org/3/movie/${result?.id}/videos?api_key=${process.env.REACT_APP_API_KEY}`);
+            setTrailerLink(resd.data.results[0]?.key)
+        }
+        getTrailer()
+    }, [])
     return (
-        <div className="cursor-pointer p-3 ">
+        <div className="cursor-pointer p-3 " onClick={ () => setShow(!show)}>
             {
                 result && 
                 <img src={ 
@@ -15,7 +34,7 @@ const  Thumbnail = ({result}:thumbNailI)  => {
                 } height={1080} width={1920} alt="emh"/>
             }
             <div className="p-2">
-                <p className="truncate max-w-md">{result.overview}</p>
+                <p  className="truncate max-w-md">{result.overview}</p>
                 <h2 className="mt-1 text-2xl text-white">
                     {
                         result.title || result.original_name
@@ -29,6 +48,9 @@ const  Thumbnail = ({result}:thumbNailI)  => {
                 </p>
                 <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
             </div>
+            {
+                show && <Youtube videoId={trailerLink}  />
+            }
         </div>
     )
 }
